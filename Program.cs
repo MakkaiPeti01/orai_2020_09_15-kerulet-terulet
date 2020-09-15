57b6e60438f422dc4425a23302d14b031876e390
MakkaiPeti01 <PetyaPower@gmail.com>
2020-09-15 10:18:54 +0200

    orai

--- Program.cs
+++ Program.cs
@@ -6,11 +6,23 @@ using System.Threading.Tasks;
 
 namespace orai_2020_09_15
 {
+    class Kor
+    {
+        public int r;
+        public double Kerulet()
+        {
+            return 2 * r * Math.PI;
+        }
+        public double Terulet()
+        {
+            return Math.Pow(r,2) * Math.PI;
+        }
+    }
     class Teglalap
     {
         public int a, b;
         public int Kerulet()
-        {
+        {           
             return 2 * (a + b);
         }
         public int Terulet()
@@ -22,11 +34,16 @@ namespace orai_2020_09_15
     {
         static void Main(string[] args)
         {
-            Teglalap oldal = new Teglalap();           
-            oldal.a = 10;
-            oldal.b = 15;
+            Teglalap oldal = new Teglalap();
+            oldal.a = int.Parse(Console.ReadLine());
+            oldal.b = int.Parse(Console.ReadLine());
             Console.WriteLine("A téglalap kerülete: {0}",oldal.Kerulet());
             Console.WriteLine("A téglalap területe: {0}", oldal.Terulet());
+            Console.WriteLine();
+            Kor er = new Kor();
+            er.r = int.Parse(Console.ReadLine());
+            Console.WriteLine("A kör kerülete: {0}", er.Kerulet());
+            Console.WriteLine("A kör területe: {0}", er.Terulet());
             Console.ReadKey();
         }
     }
