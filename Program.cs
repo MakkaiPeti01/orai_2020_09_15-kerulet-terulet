1a6f567e96313be7780abb5a8d62eeb59b34f70f
MakkaiPeti01 <PetyaPower@gmail.com>
2020-09-15 09:58:02 +0200

    terulet,kerulet

--- Program.cs
+++ Program.cs
@@ -6,10 +6,28 @@ using System.Threading.Tasks;
 
 namespace orai_2020_09_15
 {
+    class Teglalap
+    {
+        public int a, b;
+        public int Kerulet()
+        {
+            return 2 * (a + b);
+        }
+        public int Terulet()
+        {
+            return a * b;
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            Teglalap oldal = new Teglalap();           
+            oldal.a = 10;
+            oldal.b = 15;
+            Console.WriteLine("A téglalap kerülete: {0}",oldal.Kerulet());
+            Console.WriteLine("A téglalap területe: {0}", oldal.Terulet());
+            Console.ReadKey();
         }
     }
 }
